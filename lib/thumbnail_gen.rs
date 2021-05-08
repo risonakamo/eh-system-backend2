060393868e620da50214b091f03fc8e0692f9b2f
risonakamo <hmykanae@gmail.com>
2021-05-08 13:57:28 -0400

    trying async but not making sense

--- lib/thumbnail_gen.rs
+++ lib/thumbnail_gen.rs
@@ -1,9 +1,9 @@
 /// thumbnail generation library
 
 use std::path::{Path,PathBuf};
-use std::fs::{read_dir,DirEntry};
+use std::fs::{read_dir,DirEntry,create_dir_all};
 use std::io;
-use tokio::process::Command;
+use tokio::process::{Command,Child};
 
 /// generate thumbnails for a single directory, relative to a base path. reconstructs the path structure
 /// of basepath+imagepath in the thumbnail base path, and places images in that location.
@@ -27,28 +27,44 @@ pub fn genAlbumThumbnails(basepath:&str,thumbnailBasePath:&str,imagepath:&str)
         return Some(thepath);
     }).collect();
 
-    println!("{:#?}",imagePaths);
+    if imagePaths.len()==0
+    {
+        return;
+    }
+
+    create_dir_all(&thumbnailOutputDir).unwrap();
+
+    let mut tasks=vec![];
+
+    for x in imagePaths
+    {
+        tasks.push(genThumbnail(
+            x.to_str().unwrap().to_string(),
+            thumbnailOutputDir.to_str().unwrap(),
+            200
+        ));
+    }
+
+
 }
 
 /// generate a thumbnail for the given target image and place it into the target output folder,
 /// with the same name as the original, but with png file extension.
-async fn genThumbnail(target:&str,outputDir:&str,height:u32)
+async fn genThumbnail(target:String,outputDir:&str,height:u32)->Child
 {
     let outputPath:String=Path::new(outputDir).join(
-        Path::new(target).file_stem().unwrap()
+        Path::new(&target).file_stem().unwrap()
     ).with_extension("png").to_str().unwrap().to_string();
 
-    let mut thumbnailjob=Command::new("ffmpeg")
+    return Command::new("ffmpeg")
         .arg("-y")
         .args(&["-loglevel","error"])
-        .args(&["-i",target])
+        .args(&["-i",&target])
         .args(&["-vf",&format!("scale={}:-1",height)])
         .args(&["-frames:v","1"])
         .arg(outputPath)
         .spawn()
         .unwrap();
-
-    thumbnailjob.wait().await.unwrap();
 }
 
 pub mod test
