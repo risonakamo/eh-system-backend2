cea1e53fbe4f880081b93cf5f838d21501f6a0da
risonakamo <hmykanae@gmail.com>
2021-05-08 16:19:44 -0400

    thumnail gen process but still stuck

--- lib/thumbnail_gen.rs
+++ lib/thumbnail_gen.rs
@@ -3,11 +3,13 @@
 use std::path::{Path,PathBuf};
 use std::fs::{read_dir,DirEntry,create_dir_all};
 use std::io;
-use tokio::process::{Command,Child};
+use tokio::process::{Command};
+use tokio::task::JoinHandle;
+use tokio::join;
 
 /// generate thumbnails for a single directory, relative to a base path. reconstructs the path structure
 /// of basepath+imagepath in the thumbnail base path, and places images in that location.
-pub fn genAlbumThumbnails(basepath:&str,thumbnailBasePath:&str,imagepath:&str)
+pub async fn genAlbumThumbnails(basepath:&str,thumbnailBasePath:&str,imagepath:&str)
 {
     // full path to the target image dir
     let fullImagePath:PathBuf=Path::new(basepath).join(imagepath);
@@ -34,29 +36,45 @@ pub fn genAlbumThumbnails(basepath:&str,thumbnailBasePath:&str,imagepath:&str)
 
     create_dir_all(&thumbnailOutputDir).unwrap();
 
-    let mut tasks=vec![];
+    // TODO: generate thumbnails in batches so dont break system with many spawned processes
+    genMultipleThumbnails(
+        imagePaths,
+        thumbnailOutputDir.to_str().unwrap(),
+        4,
+        200
+    ).await;
+}
 
-    for x in imagePaths
+/// generate thumbnails for a vector of targets. places all into the same output directory. specify batch
+/// size for number of simultaneous ffmpeg processes.
+async fn genMultipleThumbnails(targets:Vec<PathBuf>,outputdir:&'static str,batchSize:u32,height:u32)
+{
+    let thumbnailTasks:Vec<JoinHandle<()>>=targets.into_iter()
+    .map(|x:PathBuf|->JoinHandle<()> {
+        return tokio::spawn(async move {
+            genThumbnail(
+                x.to_str().unwrap(),
+                outputdir,
+                height
+            ).await;
+        });
+    }).collect();
+
+    for x in thumbnailTasks
     {
-        tasks.push(genThumbnail(
-            x.to_str().unwrap().to_string(),
-            thumbnailOutputDir.to_str().unwrap(),
-            200
-        ));
+        join!(x);
     }
-
-
 }
 
 /// generate a thumbnail for the given target image and place it into the target output folder,
 /// with the same name as the original, but with png file extension.
-async fn genThumbnail(target:String,outputDir:&str,height:u32)->Child
+async fn genThumbnail(target:&str,outputDir:&str,height:u32)
 {
-    let outputPath:String=Path::new(outputDir).join(
+    let outputPath:String=Path::new(&outputDir).join(
         Path::new(&target).file_stem().unwrap()
     ).with_extension("png").to_str().unwrap().to_string();
 
-    return Command::new("ffmpeg")
+    let mut ffmpeg=Command::new("ffmpeg")
         .arg("-y")
         .args(&["-loglevel","error"])
         .args(&["-i",&target])
@@ -65,6 +83,8 @@ async fn genThumbnail(target:String,outputDir:&str,height:u32)->Child
         .arg(outputPath)
         .spawn()
         .unwrap();
+
+    ffmpeg.wait().await.unwrap();
 }
 
 pub mod test
