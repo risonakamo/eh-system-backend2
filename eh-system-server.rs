cfc11b781a6a30f4284deabca35c05b70e9cda8b
risonakamo <hmykanae@gmail.com>
2021-05-10 23:05:22 -0400

    static vars for now

--- eh-system-server.rs
+++ eh-system-server.rs
@@ -9,6 +9,9 @@ use eh_system::album_info::{getAlbumInfo};
 use eh_system::types::album_info_types::AlbumInfo;
 use eh_system::url_conversion::rebaseAlbumInfos;
 
+static IMAGE_DATA_PATH:&str=r"C:\Users\ktkm\Desktop\h\cg";
+static THUMBNAIL_API_PATH:&str="/thumbnaildata";
+
 #[post("/get-album",format="text/plain",data="<albumpath>")]
 fn getAlbum_api(albumpath:String)
 {
@@ -19,13 +22,13 @@ fn getAlbum_api(albumpath:String)
 fn getAlbumInfo_api(albumpath:String)->JsonValue
 {
     let albuminfo:Vec<AlbumInfo>=getAlbumInfo(
-        r"testfiles2",
+        IMAGE_DATA_PATH,
         &albumpath
     );
 
     let albuminfoFixedUrls:Vec<AlbumInfo>=rebaseAlbumInfos(
-        r"testfiles2",
-        "/thumbnaildata",
+        IMAGE_DATA_PATH,
+        THUMBNAIL_API_PATH,
         albuminfo
     );
 
@@ -35,6 +38,6 @@ fn getAlbumInfo_api(albumpath:String)->JsonValue
 fn main()
 {
     rocket::ignite()
-        .mount("/",routes![getAlbum_api,getAlbumInfo_api])
+    .mount("/",routes![getAlbum_api,getAlbumInfo_api])
         .launch();
 }
